d85a051d82be9ba697d8613fc1bfde6481590d60
harish <koninti9@gmail.com>
2021-01-27 18:29:49 +0530

    adding files

---
+++ App.js
@@ -0,0 +1,9 @@
+//Importing React
+import React from "react";
+
+//This Component wrapping our all screens in it.
+import MainScreen from "./Routes";
+
+export default function App() {
+  return <MainScreen />;
+}
